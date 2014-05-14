b3d5d4dce6885665a00ad28e8774efa19ea346af
Jesse Weed <jesse@jesseweed.com>
2014-05-14 12:16:14 -0700

    fix url setting

--- config/environment/production.js
+++ config/environment/production.js
@@ -3,23 +3,10 @@ var colors = require('colors'),
 
 module.exports = function (app) {
 
-  var interfaces = os.networkInterfaces();
-  var addresses = [];
-  for (k in interfaces) {
-    for (k2 in interfaces[k]) {
-      var address = interfaces[k][k2];
-      if (address.family == 'IPv4' && !address.internal) {
-        addresses.push(address.address)
-      }
-    }
-  }
-
   var tesla = require('../../lib/tesla')(app);
 
-  if ( typeof addresses[0] === 'undefined' ) addresses[0] = 'http://placeanderson.herokuapp.com/'
-
   // global settings
-  app.site.domain = addresses[0];
+  app.site.domain = 'http://placeanderson.herokuapp.com/';
   app.site.environment = "production";
   app.site.url = app.config.protocol + app.site.domain + '/'; // base url
 
