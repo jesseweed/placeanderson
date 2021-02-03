849effb6d0d29c9bd429d0abb43ccc407a5cdb8b
Jesse Weed <code@jesseweed.io>
2021-02-03 00:16:49 -0800

    fix https

--- config/environment/production.js
+++ config/environment/production.js
@@ -7,15 +7,16 @@ module.exports = function (app) {
 
   // global settings
   app.site.domain = 'placeanderson.herokuapp.com';
-  app.site.environment = "production";
+  app.site.environment = 'production';
+  app.config.protocol = 'https';
   app.site.url = app.config.protocol + app.site.domain + '/'; // base url
 
   // directories
   app.site.dir = {
-    css : app.site.url + "css/",
-    img : app.site.url + "img/",
-    lib : app.site.url + "lib/",
-    js : app.site.url + "js/"
+    css : app.site.url + 'css/',
+    img : app.site.url + 'img/',
+    lib : app.site.url + 'lib/',
+    js : app.site.url + 'js/'
   };
 
   tesla.log('INFO:'.blue + ' ' + app.site.environment + ' config loaded' );
