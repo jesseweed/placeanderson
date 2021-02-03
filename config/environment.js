8a2308a16258beed42a82e3a206bb393f4cc9c9e
Jesse Weed <code@jesseweed.io>
2021-02-02 23:33:19 -0800

    misc cleanup

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,2 @@
-var _ = require('underscore');
-
 // Load app configuration
-module.exports = _.extend( require(__dirname + '/../config/environment/' + process.env.NODE_ENV + '.js') || {} );
+module.exports = require(__dirname + '/../config/environment/' + process.env.NODE_ENV + '.js') || {} ;
